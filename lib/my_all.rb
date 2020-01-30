6889746a914180f80959c9e953e9aaeef425a04a
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 21:00:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,4 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
